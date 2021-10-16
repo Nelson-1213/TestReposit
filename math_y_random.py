ed05ae865e568844f554f3b62ef4bf4a17fc1826
Nelson-1213 <63508907+Nelson-1213@users.noreply.github.com>
2021-10-15 22:16:04 -0500

    Add files via upload

---
+++ math_y_random.py
@@ -0,0 +1,18 @@
+from math import hypot
+import random
+random.random()
+random.choice(['red','green','grown','blue','black','cian','white','yellow',])
+random.choices(['red','green','grown','blue','black','cian','white','yellow'],k=3)
+import math
+math.hypot(3,2)
+True and True
+False and True
+False or True
+not True
+1 == 5
+3 > 4 < 5 and 3 < 2 or 2 != 4 and 4 == 2 + 2 or not True
+a = 6
+if a < 0:
+    print("El numero es menor que 10")
+else:
+    print("El numero es negativo")
